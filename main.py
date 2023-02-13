607676cbdcfc4f337ec8a274309fe736d883196f
kostikgrach <118543790+kostikgrach@users.noreply.github.com>
2023-02-13 19:14:20 +0300

    Add files via upload
    Тренировки в полёте

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    param = {'username': "Ученик Яндекс.Лицея", 'title': 'Домашняя страница'}
+    return render_template('index.html', **param)
+
+
+@app.route('/index/<title>')
+def index1(title):
+    return render_template('base.html', title1=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
